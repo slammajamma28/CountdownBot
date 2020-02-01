c6754c00e5627f11bcaec18a8b43e1bd62a94a6e
slammajamma28 <slammajamma28@gmail.com>
2020-02-01 11:25:21 -0500

    Start $create command. Changed $hello to $greet (per API example)

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import discord
+import configparser 
+import asyncio
 
 client = discord.Client()
+
+config = configparser.RawConfigParser()
+config.read('token.properties')
 
 @client.event
 async def on_ready():
@@ -11,7 +16,33 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$greet'):
+        channel = message.channel
+        await channel.send('Say hello!')
 
-client.run(bot.token)
+        def check(m):
+            return m.content == 'hello' and m.channel == channel
+
+        msg = await client.wait_for('message', check=check)
+        await channel.send('Hello {.author}!'.format(msg))
+
+    if message.content.startswith('$create'):
+        channel = message.channel
+        await message.channel.send('Event name?')
+        def check(m):
+            return m.channel == channel
+
+        try:
+            event_name = await client.wait_for('message', timeout=10.0, check=check)
+        except asyncio.TimeoutError:
+            await message.channel.send('Oooh a pretty butterfly! [timeout for input]')
+        else:
+            await message.channel.send('Event name is: {.content}'.format(event_name))
+
+
+# Need a local file called token.properties
+# [Token]
+# value=TOKEN_HERE
+# Don't forget to add to .git/info/exclude
+# Because we should not check in the token
+client.run(config.get('Token', 'value'))
